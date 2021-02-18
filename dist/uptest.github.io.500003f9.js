1e24bfe83cd9ce5ba0968f7e5657b6a03eccb704
Julia Lopatka <julialopatka@MacBook-Pro-Julia.local>
2021-02-18 19:07:20 +0200

    content adaptation for mobile devices

--- dist/uptest.github.io.500003f9.js
+++ dist/uptest.github.io.500003f9.js
@@ -413,6 +413,16 @@ $(window).on('load', function () {
     $(this).toggleClass('open');
     $(".main-nav__menu").toggleClass('main-nav__menu--open');
   });
+  $(".table-1__btns").on("click", function () {
+    $(".table-1__btns").toggleClass('table-1__btns--open');
+  });
+  $("a[href^='#']").click(function (e) {
+    e.preventDefault();
+    var dest = $(this).attr('href');
+    $('html,body').animate({
+      scrollTop: $(dest).offset().top
+    }, 'slow');
+  });
   $(".bottles__item").hover(function () {
     $(this).addClass('active');
   }, function () {
--- index.js
+++ index.js
@@ -6,6 +6,18 @@ $(window).on('load',function(){
         $(this).toggleClass('open')
         $(".main-nav__menu").toggleClass('main-nav__menu--open')
     });
+    $( ".table-1__btns" ).on( "click", function(){
+        $(".table-1__btns").toggleClass('table-1__btns--open')
+    });
+
+    $("a[href^='#']").click(function(e) {  
+        e.preventDefault();
+        var dest = $(this).attr('href');  
+        $('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow');
+    });
+
+
+
 
     $(".bottles__item").hover(
         function () {
